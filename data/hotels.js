6f547c00cd70e377bbb1627e819e4cd8d7a4fab4
jadexaa <10987469@uvu.edu>
2024-12-01 02:47:41 -0700

    first

---
+++ data/hotels.js
@@ -0,0 +1,66 @@
+export const hotels = [
+  
+    { 
+        "name": "CASCADE Hotel in Utah, USA",
+        "photo": "uthotel.webp",
+        "address": ["801 Richmond Park Street,", "Cascade, UT 84972"],
+        "phone": "(801) 916-7152"
+        
+    },
+
+    { 
+        "name": "CASCADE Resort in Utah, USA",
+        "photo": "utresort.webp",
+        "address": ["856 Willus Center Drive,", "Cascade, UT 86172"],
+        "phone": "(801) 582-2715"
+        
+    },
+
+    { 
+        "name": "Cascade Hotel in Florida, USA",
+        "photo": "flhotel.webp",
+        "address": ["321 Ivern Jungle Road,", "Cascade, FL 91725"],
+        "phone": "(321) 615-5128"
+        
+    },
+
+    { 
+        "name": "Cascade Resort in Florida, USA",
+        "photo": "flresort.webp",
+        "address": ["162 Shurima Lane,", "Cascade, FL 92612"],
+        "phone": "(617) 822-3600"
+        
+    },
+
+    { 
+        "name": "Cascade Hotel & Resort in Vietnam",
+        "photo": "vietnam.webp",
+        "address": ["123 Duong Le Loi,", "Cascade, Vietnam"],
+        "phone": "84 91 234 5678"
+        
+    },
+
+    { 
+        "name": "Cascade Hotel & Resort in France",
+        "photo": "france.webp",
+        "address": ["15 Rue de Rivoli,", "75004 Paris, France"],
+        "phone": "33 6 12 34 56 78"
+        
+    },
+
+    { 
+        "name": "Cascade Hotel & Resort in China",
+        "photo": "china.webp",
+        "address": ["88 Nanjing Road,", "Shanghai, China"],
+        "phone": "86 138 1234 5678"
+        
+    },
+
+    { 
+        "name": "Cascade Hotel & Resort in Dubai",
+        "photo": "dubai.webp",
+        "address": ["123 Sheikh Zayed Road", "Dubai, United Arab Emirates"],
+        "phone": "971 50 123 4567"
+        
+    },
+]
